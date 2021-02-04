e5aa0bd60112ddab9b293aab3885e00e42cad7f4
Ivan <ivan.litrenta@hotmail.com>
2021-02-04 17:18:06 +0000

    fix build

--- src/client/containers/About/About.js
+++ src/client/containers/About/About.js
@@ -1,16 +1,14 @@
 import React, { Fragment } from 'react'
 import Helmet from 'react-helmet'
-import loadable from "@loadable/component";
 
 import styles from './About.scss'
-const C = loadable(() => import(/* webpackPreload: true */ "../../letters/C"));
 const About = ()=>{
 	// console.log(styles)
 	return <Fragment>
 		<Helmet title="About" />
 		<div className={styles.about}>
 			<h2>About</h2>
-			<C />
+			<p>Lorem ipsum</p>
 		</div>
 	</Fragment>
 }
