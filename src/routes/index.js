1b7edbf44a5e8deef516f5c293a7cbf08d206e34
Ivan <ivan.litrenta@hotmail.com>
2021-02-06 17:24:14 +0000

    fix chunks name

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,20 @@
-import loadable from "@loadable/component";
+import loadable from '@loadable/component'
 
-const Home = loadable(() => import("../client/containers/Home" /* webpackChunkName: "home" */));
-const About = loadable(() => import("../client/containers/About" /* webpackChunkName: "about" */));
+const Home = loadable(() => import('../client/containers/Home'))
+const About = loadable(() => import('../client/containers/About'))
 
 const routes = [
-	{
-		component: Home,
-		path: "/",
-		exact: true
-	},
-	{
-		component: About,
-		path: "/about",
-		exact: true
-		// loadData: () => ({type:'FETCH_ABOUT', payload: {about:'Data about'}})
-	}
+		{
+			component: Home,
+			path: "/",
+			exact: true
+		},
+		{
+			component: About,
+			path: "/about",
+			exact: true,
+			loadData: () => ({type:'FETCH_ABOUT', payload: {about:'Data about'}})
+		}
 ];
 
-export default routes;
+export default routes
